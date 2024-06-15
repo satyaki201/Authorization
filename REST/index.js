9b198a9c46711c68938f0ed299ff5227d5190fd6
SATYAKI SAHA <satyaki01saha@gmail.com>
2024-06-15 09:13:00 +0000

    committed

--- REST/index.js
+++ REST/index.js
@@ -1,16 +1,38 @@
 const express=require('express');
 const data=require('./Data.json')
+const fs=require('fs');
 const app=express();
 const PORT=7000;
+
+app.use(express.urlencoded({extended:false}));
+
 app.get('/',(req,res)=>
 {
     res.send("Hi this is The Home Page");
 });
 app.get('/request',(req,res)=>{
-    return res.send(data.map(user => user.email));
+    res.json(data.map(user => user.email));
 })
+
+app.get('/api/request/:id',(req,res)=>{
+    const id=Number(req.params.id);
+    const user=data.find((user)=>user.id===id);
+    return res.json(user);
+});
+app.post('/api/request', (req, res) => {
+    const body = req.body;
+    const newUser = { ...body, id: data.length + 1 };
+    data.push(newUser);
+    fs.writeFile("./Data.json", JSON.stringify(data, null, 2), (err) => {
+        if (err) {
+            console.error('Error writing to file:', err);  // Log file write error
+            return res.status(500).json({ status: "error", message: "Failed to write data" });
+        }
+        return res.json({ status: "success", user: newUser });
+    });
+});
 
 
 app.listen(PORT,()=>{
-    console.log("App started on"+PORT);
+    console.log("App started on "+PORT);
 });
